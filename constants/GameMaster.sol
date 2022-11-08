da0b532e406d2221ecadd18ffa5e1d15d2880b11
Apoorv <apoorva@uniris.io>
2022-11-09 00:21:13 +0530

    improve contract

--- constants/GameMaster.sol
+++ constants/GameMaster.sol
@@ -2,14 +2,13 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract GameMaster {
+contract GameElements {
     enum GameStatus {
         inactive,
         queued,
         active,
         ended
     }
-
     enum UnitType {
         infantry,
         tank,
@@ -27,57 +26,77 @@ contract GameMaster {
         moving,
         combat
     }
+}
 
-    struct User {
-        address userAddress;
-    }
-
+contract GameMaster is GameElements {
     uint256 private matchCount = 0;
 
     struct Match {
         uint32 id;
-        address playerA;
-        address playerB;
+        Player playerA;
+        Player playerB;
         GameStatus status;
-        uint16[][] grid;
+        uint16[][][] grid;
         //  gamegrid
         //  empty:0 infantry:11 tank:12 drone:13   //fogofwar //
         //  UserB: infantry:21 tank:22 drone:23
     }
 
     struct Unit {
-        uint16 unitId;
+        uint256 unitId;
         UnitType unitType;
         uint16 hp;
         uint16 attack;
         uint16 armor;
         ActionState currentAction;
-        //uint256 attackRange;
-        //uint256 ammunition;
-        //uint256 range;
-        //uint256 visibilityRange;
     }
 
-    struct Account {
-        //banner
+    struct Player {
         address owner;
         Rank rank;
-        uint16 experience;
         Unit[] Army;
+        uint256 unit_id_counter;
     }
 
-    mapping(address => Account) public addressToAccount;
+    mapping(address => Player) public addressToPlayer;
     mapping(uint256 => Match) public gameIdToMatch;
+
+    // user a first time visits website we must init a player for him
+    // and allocate 3 infantries
+    function initPlayer() public {
+        Player memory newPlayer;
+        newPlayer.owner = msg.sender;
+        newPlayer.rank = Rank.Sergeant;
+        newPlayer.unit_id_counter = 0;
+        Unit memory newUnit = initInfantary(newPlayer.unit_id_counter);
+        newPlayer.Army[newPlayer.unit_id_counter] = newUnit;
+        newPlayer.unit_id_counter++;
+        addressToPlayer[msg.sender] = newPlayer;
+    }
+
+    function initInfantary(uint256 _unitId)
+        private
+        pure
+        returns (Unit memory newUnit)
+    {
+        newUnit.unitType = UnitType.infantry;
+        newUnit.hp = 10;
+        newUnit.attack = 1;
+        newUnit.armor = 0;
+        newUnit.currentAction = ActionState.idle;
+        newUnit.unitId = _unitId;
+        return newUnit;
+    }
 
     function matchFactory() public returns (uint256) {
         //initialize new match and empty grid
         Match memory newMatch;
-        uint16[][] memory newGrid;
+        uint16[][][] memory newGrid;
         uint256 matchId = matchCount;
 
         //add defualt values to newMatch
         newMatch.id = uint32(matchId);
-        newMatch.playerA = msg.sender;
+        newMatch.playerA = addressToPlayer[msg.sender];
         newMatch.status = GameStatus.queued;
         newMatch.grid = newGrid;
         gameIdToMatch[matchCount] = newMatch;
@@ -88,18 +107,40 @@ contract GameMaster {
     }
 
     function joinMatch(uint256 matchId) public {
-        gameIdToMatch[matchId].playerB = msg.sender;
+        gameIdToMatch[matchId].playerB = addressToPlayer[msg.sender];
+        initGame(matchId);
+    }
+
+    function initGame(uint256 matchId) private {
+        // assign player a unit pos 00  and b positon 100 ,100
+        // player 1 and player b is 2
+        gameIdToMatch[matchId].grid[1][0][0] = uint16(
+            gameIdToMatch[matchId].playerA.Army[0].unitId
+        );
+
+        gameIdToMatch[matchId].grid[2][100][100] = uint16(
+            gameIdToMatch[matchId].playerB.Army[0].unitId
+        );
         gameIdToMatch[matchId].status = GameStatus.active;
     }
 
-    //todo calculate battleLogic
-    function resolveBattle(uint256 unitOneId, uint256 unitTwoId)
+    function read_state(uint256 matchId)
         public
-        returns (uint256 winnerId)
+        view
+        returns (Match memory matchAB)
     {
-        //battle logic needed here
-        //destroy losing force, reward exp to wiinning unit
-        //returns winner
-        return unitOneId;
+        matchAB = gameIdToMatch[matchId];
+        return matchAB;
     }
+
+    // todo calculate battleLogic
+    // function resolveBattle(uint256 unitOneId, uint256 unitTwoId)
+    //     public
+    //     returns (uint256 winnerId)
+    // {
+    //     //battle logic needed here
+    //     //destroy losing force, reward exp to wiinning unit
+    //     //returns winner
+    //     return unitOneId;
+    // }
 }
