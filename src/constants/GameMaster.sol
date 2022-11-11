2ae85637bdba9e85a387aec2f87e1ac7c58adae5
Markeljan <Markeljan19@gmail.com>
2022-11-11 13:36:04 -0500

    updated Master contract

--- src/constants/GameMaster.sol
+++ src/constants/GameMaster.sol
@@ -1,137 +1,161 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.7.0 <0.9.0;
-
-contract GameElements {
-    enum GameStatus {
-        inactive,
-        queued,
-        active,
-        ended
-    }
-    enum UnitType {
-        infantry,
-        tank,
-        drone
-    }
-
-    enum Rank {
-        Sergeant,
-        Lieutenant,
-        Captain
-    }
-
-    enum ActionState {
-        idle,
-        moving,
-        combat
-    }
-}
+pragma solidity 0.8.14;
+import "./GameElements.sol";
 
 contract GameMaster is GameElements {
-    uint256 private matchCount = 0;
+    //  zero reserved
+    uint256 internal matchCount = 1;
+    uint256 internal unitGlobalId = 1;
+
+    uint256[10][10] public gameGrid;
 
     struct Match {
-        uint32 id;
+        uint256 id;
         Player playerA;
         Player playerB;
         GameStatus status;
-        uint16[][][] grid;
-        //  gamegrid
-        //  empty:0 infantry:11 tank:12 drone:13   //fogofwar //
-        //  UserB: infantry:21 tank:22 drone:23
+        uint256[10][10] gameGrid;
     }
 
     struct Unit {
-        uint256 unitId;
-        UnitType unitType;
-        uint16 hp;
-        uint16 attack;
-        uint16 armor;
+        uint256 id;
+        UnitTypes unitType;
+        uint256 hp;
+        uint256 attack;
+        uint256 armor;
         ActionState currentAction;
+        address owner;
+        uint256 count;
+        // Match Info
+        uint x;
+        uint y;
+        // uint matchId;
+        uint regenerationTime;
+        uint establishTimeTOReach;
+        InterMatchStatus available;
     }
 
     struct Player {
         address owner;
         Rank rank;
-        Unit[] Army;
-        uint256 unit_id_counter;
+        // uuid for Player army=> unique ID
+        // starts with 1, 0 reserved
+        uint256 playerSpecficUnitId;
     }
 
-    mapping(address => Player) public addressToPlayer;
-    mapping(uint256 => Match) public gameIdToMatch;
+    mapping(address => Player) internal addressToPlayer;
+    mapping(uint256 => Match) internal gameIdToMatch;
+    mapping(address => mapping(uint256 => Unit)) internal ownerToIdToUnit;
+    mapping(uint256 => Unit) internal unitIdToUnit;
 
-    // user a first time visits website we must init a player for him
-    // and allocate 3 infantries
+    event displayInitPlayer(Player a);
+
+    /// @dev first time player initialisation
+    /// with 3 infantary units
     function initPlayer() public {
-        Player memory newPlayer;
+        Player memory newPlayer = addressToPlayer[msg.sender];
+        require(newPlayer.rank != Rank.none);
+
         newPlayer.owner = msg.sender;
         newPlayer.rank = Rank.Sergeant;
-        newPlayer.unit_id_counter = 0;
-        Unit memory newUnit = initInfantary(newPlayer.unit_id_counter);
-        newPlayer.Army[newPlayer.unit_id_counter] = newUnit;
-        newPlayer.unit_id_counter++;
+        newPlayer.playerSpecficUnitId = 1;
+        initInfantary(newPlayer.playerSpecficUnitId, msg.sender);
+        newPlayer.playerSpecficUnitId++;
         addressToPlayer[msg.sender] = newPlayer;
+        emit displayInitPlayer(newPlayer);
     }
 
-    function initInfantary(uint256 _unitId)
-        private
-        pure
-        returns (Unit memory newUnit)
-    {
-        newUnit.unitType = UnitType.infantry;
+    /// @dev init first 3 infantary units
+    /// mapped to ownerToIdToUnit
+    function initInfantary(uint256 _id, address _owner) private {
+        Unit memory newUnit;
+        newUnit.unitType = UnitTypes.infantry;
         newUnit.hp = 10;
         newUnit.attack = 1;
         newUnit.armor = 0;
         newUnit.currentAction = ActionState.idle;
-        newUnit.unitId = _unitId;
-        return newUnit;
+        newUnit.id = _id;
+        newUnit.count = 3;
+        newUnit.owner = _owner;
+        newUnit.x = 0;
+        newUnit.y = 0;
+        ownerToIdToUnit[_owner][_id] = newUnit;
     }
 
-    function matchFactory() public returns (uint256) {
-        //initialize new match and empty grid
-        Match memory newMatch;
-        uint16[][][] memory newGrid;
-        uint256 matchId = matchCount;
+    // function matchFactory() public returns (uint256  matchId) {
 
-        //add defualt values to newMatch
-        newMatch.id = uint32(matchId);
+    // }
+
+    function newGame() public returns (uint256 matchId) {
+        //initialize new match
+        Match memory newMatch;
+        matchId = GameMaster.matchCount;
+
+        newMatch.id = GameMaster.matchCount;
         newMatch.playerA = addressToPlayer[msg.sender];
         newMatch.status = GameStatus.queued;
-        newMatch.grid = newGrid;
         gameIdToMatch[matchCount] = newMatch;
-
+        // TODO init army units
         //increment matchCounter
-        matchCount++;
+        GameMaster.matchCount++;
         return matchId;
     }
 
     function joinMatch(uint256 matchId) public {
+        if (addressToPlayer[msg.sender].playerSpecficUnitId == 0) {
+            initPlayer();
+        }
         gameIdToMatch[matchId].playerB = addressToPlayer[msg.sender];
-        initGame(matchId);
+        // TODO init army units
     }
 
-    function initGame(uint256 matchId) private {
-        // assign player a unit pos 00  and b positon 100 ,100
-        // player 1 and player b is 2
-        gameIdToMatch[matchId].grid[1][0][0] = uint16(
-            gameIdToMatch[matchId].playerA.Army[0].unitId
-        );
-
-        gameIdToMatch[matchId].grid[2][100][100] = uint16(
-            gameIdToMatch[matchId].playerB.Army[0].unitId
-        );
-        gameIdToMatch[matchId].status = GameStatus.active;
+    function listArmy() public view returns (Unit[] memory armyList) {
+        Player memory tempPlayer = addressToPlayer[msg.sender];
+        uint army_size = tempPlayer.playerSpecficUnitId - 2;
+        Unit[] memory army_list = new Unit[](army_size);
+        for (uint i = 1; i < tempPlayer.playerSpecficUnitId; i++) {
+            armyList[i - 1] = ownerToIdToUnit[msg.sender][i];
+        }
+        return army_list;
     }
 
-    function read_state(uint256 matchId)
+    function selectArmy(uint256 matchId, uint armyUnitId)
         public
-        view
-        returns (Match memory matchAB)
+        returns (Unit memory army)
     {
-        matchAB = gameIdToMatch[matchId];
-        return matchAB;
+        ownerToIdToUnit[msg.sender][armyUnitId].available = InterMatchStatus
+            .inWar;
+        army = ownerToIdToUnit[msg.sender][armyUnitId];
+        return army;
     }
+
+    //     Unit[10][10] public grid;
+    //  function abc() public  {
+    // //   mapping(Unit => uint) memory abc;
+    //     Unit memory newUnit;
+    //     newUnit.hp = 10;
+    //     newUnit.attack = 1;
+    //     newUnit.armor = 0;
+    //     newUnit.currentAction = ActionState.idle;
+    //     newUnit.id = 0;
+    //     newUnit.count = 3;
+    //     newUnit.owner = msg.sender;
+    //     newUnit.x = 0;
+    //     newUnit.y = 0;
+    //     newUnit.matchId = 0;
+
+    //         grid[0][0] = newUnit;
+    // }
+
+    // function read_state(uint256 matchId)
+    //     public
+    //     view
+    //     returns (Match memory matchAB)
+    // {
+    //     matchAB = gameIdToMatch[matchId];
+    //     return matchAB;
+    // }
 
     // todo calculate battleLogic
     // function resolveBattle(uint256 unitOneId, uint256 unitTwoId)
